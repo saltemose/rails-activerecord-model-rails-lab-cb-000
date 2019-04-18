d2ff8bdd7ac1fea3eac0b25ddfd6023dcdf385a4
Sean Altemose <sean@seanaltemose.com>
2019-04-18 16:26:01 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180912171417) do
+
+  create_table "create_students", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,12 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
+Student.create(first_name: "Daenerys", last_name: "Targaryen" => "Daenerys Targaryen")
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ActiveRecord::Schema.define(version: 1) do
+   create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+  end
